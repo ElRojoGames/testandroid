2efeea9f856d810a054efd5beac1a9e865403f3b
ElRojoGames <luis.wr10@gmail.com>
2018-04-09 13:40:11 -0500

    video 11 de CF

--- app/src/main/java/com/example/elrojo/testapplication/Activities/InsertActivity.java
+++ app/src/main/java/com/example/elrojo/testapplication/Activities/InsertActivity.java
@@ -2,14 +2,33 @@ package com.example.elrojo.testapplication.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.elrojo.testapplication.R;
 
 public class InsertActivity extends AppCompatActivity {
 
+    EditText id, name, age, email;
+    Button insert;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+
+        id = (EditText) findViewById(R.id.id);
+        name = (EditText) findViewById(R.id.name);
+        age = (EditText) findViewById(R.id.age);
+        email = (EditText) findViewById(R.id.email);
+        insert = (Button) findViewById(R.id.insert);
+
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
